0d85565ee2ead77a731cc744c9312026d37b08e8
w2ex <thomas.petit@ecl15.ec-lyon.fr>
2017-03-30 22:04:17 +0200

    Update points.cpp

--- points.cpp
+++ points.cpp
@@ -1,12 +1,12 @@
 #include "points.h"
 
-Points::Points(int X, int Y)
+Points::Points(int X, int Y) // constructeur
 {
     x=X;
     y=Y;
 }
 
-Points::getx(){
+Points::getx(){ // classique
     return x;
 }
 
@@ -15,10 +15,10 @@ Points::gety(){
 }
 
 bool Points::colineaires(Points *a, Points *b){
-    if ((a->gety()==b->gety()) && (a->gety()==y)){
+    if ((a->gety()==b->gety()) && (a->gety()==y)){ // première condition pour éviter la division par zéro ensuite
                 return true;
     }
-    else if(((a->getx()-x))/((float)(a->gety()-y))==((a->getx()-b->getx())/(float)(a->gety()-b->gety()))){
+    else if(((a->getx()-x))/((float)(a->gety()-y))==((a->getx()-b->getx())/(float)(a->gety()-b->gety()))){ // classique test de colinéarité
         return true;
     }
     return false;
